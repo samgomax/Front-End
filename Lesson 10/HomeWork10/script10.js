43d422f7128fc0933758ca7058fe8bacc0a2f218
Maksym Samohorodskyi <maxxxym300887@gmail.com>
2022-12-04 19:22:18 +0100

    HomeWork10

---
+++ Lesson 10/HomeWork10/script10.js
@@ -0,0 +1,25 @@
+Task_1
+
+function getArray(a){
+   let result = [];
+   for(i =a.length-1; i >=0; i--){
+        result.push(a[i]);
+    }return result
+}
+let a =[11,22]; 
+console.log(getArray(a));
+document.write(([...document.getElementsByTagName("a")].map(i=>i.href)).join("<br>"))
+Task_2
+
+let url = document.getElementsByTagName('a');
+console.log(url);
+function urlArray(){
+    document.getElementsByName('a')
+}
+
+
+Task_5
+
+
+
+
