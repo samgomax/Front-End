7c83e6c77ecf37ea1b43aa79470ca50132e3f5a9
Maksym_S <maxxxym300887@gmail.com>
2023-03-05 12:09:10 +0100

    Nav_Menu

---
+++ Lesson21_PRO.NAV_Menu/scripts/script.js
@@ -0,0 +1,4 @@
+const menu_icon = document.querySelector('#menu_icon');
+const nav_menu = document.querySelector('.nav_menu');
+
+menu_icon.addEventListener('click', () => nav_menu.classList.toggle('active'))
