87c7d6e6783f58818dbb2ade3de42e5341e977ce
Maksym_S <maxxxym300887@gmail.com>
2023-03-07 10:35:13 +0100

    popup window

--- Lesson21_PRO.NAV_Menu/scripts/script.js
+++ Lesson21_PRO.NAV_Menu/scripts/script.js
@@ -2,3 +2,15 @@ const menu_icon = document.querySelector('#menu_icon');
 const nav_menu = document.querySelector('.nav_menu');
 
 menu_icon.addEventListener('click', () => nav_menu.classList.toggle('active'))
+
+
+// при клике на аккаунт добавлять div.popup  класс active
+//при клике на кретстик удалять класс activ
+
+const accountBtn = document.querySelector('.account');
+const popup = document.querySelector('.popup');
+const crossIcon = document.querySelector('.cross_icon')
+
+accountBtn.addEventListener('click', () => popup.classList.add('window'));
+
+crossIcon.addEventListener('click', () => popup.classList.remove('window'));
