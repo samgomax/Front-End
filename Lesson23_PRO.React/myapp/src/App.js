d00fce4ef38d9ddaa606fedf19a0537d532f28f5
Maksym_S <maxxxym300887@gmail.com>
2023-03-13 21:57:55 +0100

    First React app

---
+++ Lesson23_PRO.React/myapp/src/App.js
@@ -0,0 +1,18 @@
+function App() {
+
+  let myname = 'Maksym';
+  let lastname = 'Samohorodskyi'
+
+  return (
+    <div>
+      <h1>Заголовок</h1>
+      <p>Lorem wwegagerghe efbvfbhethsfeb rgrgehd</p>
+      <p>My name is { myname }</p>
+      <p>My last name {lastname}</p>
+    </div>
+  );
+}
+
+export default App;
+
+// JSX - синтаксическое расширение, позволяющее писать html внутри js
